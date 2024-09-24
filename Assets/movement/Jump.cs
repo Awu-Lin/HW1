bf4a3e840016d698f6e2f5cd752a4745a6e00c50
Awu_Lin <zil037@ucsd.edu>
2024-09-24 09:03:53 +0800

    Step: Try some UI, not work
    hummmm try something new

--- Assets/movement/Jump.cs
+++ Assets/movement/Jump.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Jump : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public float jumpSpeed = 2f;
-    private bool isGround = true;
+    public Vector3 initialPosition; 
+    public Text failText; 
+
     Vector3 Amount;
     Rigidbody rb;
 
@@ -14,21 +17,21 @@ public class Jump : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position; 
+        failText.gameObject.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         Amount = moveDirection * moveSpeed * Time.deltaTime;
 
+        
         if (Input.GetKeyDown(KeyCode.J))
         {
-            if (isGround)
-            {
-                rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); 
-                isGround = false; 
-            }
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); 
         }
     }
 
@@ -37,11 +40,23 @@ public class Jump : MonoBehaviour
         rb.MovePosition(rb.position + Amount);
     }
 
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Obstacle") 
         {
-            isGround = true; 
+            Fail();
         }
+    }
+
+    private void Fail()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.position = initialPosition;
+        failText.text = "You failed";
+        failText.color = Color.red;
+        failText.fontSize = 64; 
+        failText.gameObject.SetActive(true);
     }
 }
